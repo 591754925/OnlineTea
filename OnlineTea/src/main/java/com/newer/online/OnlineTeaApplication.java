15cdaabcda470b602bcef1597ec415b7ad8c3c92
yhj <huanjin.yuan@foxmail.com>
2019-03-20 20:44:01 +0800

    添加项目OnlineTea，选题：喜茶

---
+++ OnlineTea/src/main/java/com/newer/online/OnlineTeaApplication.java
@@ -0,0 +1,13 @@
+package com.newer.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTeaApplication.class, args);
+	}
+
+}
